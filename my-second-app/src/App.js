795e82b628e87565a9b0e51c96bde21aeeabcced
eltwor <tworkowskaeliza@gmail.com>
2022-02-17 22:58:28 +0100

    added more stuff

--- my-second-app/src/App.js
+++ my-second-app/src/App.js
@@ -1,19 +1,20 @@
-import { Route } from "react-router-dom";
-import AllMeetupsPage from "./pages/AllMeetups";
-import FavoritesPage from "./pages/Favorites";
-import NewMeetupPage from "./pages/NewMeetup";
+import { Route, Routes } from 'react-router-dom';
 
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
 
 function App() {
-  return <div>
-    <Route path='/'>
-      <AllMeetupsPage/>
-    </Route>
-    <Route path='/new-meetup'>
-      <NewMeetupPage/>
-    </Route>
-    <FavoritesPage></FavoritesPage>
-  </div>
+  return (
+    <Layout>
+  <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
