decbfa370feab7829b005b9fa012b8705bba9a2f
eltwor <tworkowskaeliza@gmail.com>
2022-02-19 16:03:41 +0100

    finished

--- my-second-app/src/components/meetups/NewMeetupForm.js
+++ my-second-app/src/components/meetups/NewMeetupForm.js
@@ -1,37 +1,58 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
-    function submitHandler() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descInputRef = useRef();
 
-    }
-    
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDesc = descInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDesc
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
-          <div className={classes.control}>
-              <label htmlFor="title">Meetup Title</label>
-                <input type="text" required id="title" />
-          </div>
-          
-          <div className={classes.control}>
-              <label htmlFor="image">Meetup Image</label>
-                <input type="url" required id="image" />
-          </div>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
 
-          <div className={classes.control}>
-              <label htmlFor="address">Address</label>
-                <input type="text" required id="address" />
-          </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
 
-          <div className={classes.control}>
-              <label htmlFor="description">Description</label>
-                <textarea required id="description" rows='5' />
-          </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
 
-          <div className={classes.actions}>
-              <button>Add Meetup</button>
-          </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea required id="description" rows="5" ref={descInputRef} />
+        </div>
+
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
       </form>
     </Card>
   );
--- my-second-app/src/pages/NewMeetup.js
+++ my-second-app/src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  return <section>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm />
-  </section>
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-tutorial-1d6ff-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
