4ddb83d465b105e33445db215e4bab28fc788f5a
eltwor <tworkowskaeliza@gmail.com>
2024-09-20 14:38:58 +0200

    Added the GameBoard

--- tictactoe/src/components/Player.jsx
+++ tictactoe/src/components/Player.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setISEditing] = useState(false);
   function handleEditing() {
     setISEditing(editing => !editing);
   }
-  let playerTab = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let playerTab = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerTab = <input type="text" required value={name} />;
+    playerTab = <input type="text" required value={playerName} onChange={handleChange}/>;
   }
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {playerTab}
         <span className="player-symbol">{symbol}</span>
