da8838df4b79b4432d55819f7a610056c75ed4b7
eltwor <tworkowskaeliza@gmail.com>
2024-10-04 15:05:47 +0200

    Adding winners to the game

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,22 +1,90 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import { useState } from "react";
-function App() {
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
-  const [activePlayer, setActivePlayer] = useState('X');
-  function handleSelect() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns); 
+
+  
+let gameBoard = initialGameBoard;
+
+for (const turn of gameTurns) {
+  const { square, player } = turn;
+  const { row, col } = square;
+
+  gameBoard[row][col] = player;
+}
+
+  let winner = null;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+    gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+    gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelect(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelect} activePlayerSym={activePlayer}/>
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        <GameBoard onSelectSquare={handleSelect} board={gameBoard} />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- tictactoe/src/components/GameBoard.jsx
+++ tictactoe/src/components/GameBoard.jsx
@@ -1,30 +1,31 @@
-import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-export default function GameBoard({onSelectSquare, activePlayerSym}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-    function handleChange(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSym;
-            return updatedBoard;
-        });
+export default function GameBoard({ onSelectSquare, board }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // function handleChange(rowIndex, colIndex) {
+  //     setGameBoard((prevGameBoard) => {
+  //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //         updatedBoard[rowIndex][colIndex] = activePlayerSym;
+  //         return updatedBoard;
+  //     });
 
-        onSelectSquare();
-    }
+  //     onSelectSquare();
+  // }
+
+
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleChange(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
